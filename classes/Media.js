0b274246df41155817b6b43412589cbbe20b29e6
Nicolas Fabius <nicolas.fabius@nicolas.fabius-MacBook-Pro>
2024-11-04 16:20:49 -0500

    Add Media class with properties and summary method

--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,18 @@
-// create your Media class:
+class Media {
+    static totalMediaCount = 0;
+    static ALL_MEDIA = [];
 
-// uncomment below to export it:
+    constructor(title, year, genre) {
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        Media.totalMediaCount++;
+        Media.ALL_MEDIA.push(this);
+    }
+
+    summary() {
+        return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre}`;
+    }
+}
+
 module.exports = Media;
